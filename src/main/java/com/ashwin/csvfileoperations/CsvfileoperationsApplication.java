d2da73da219dab7b35eb0193e9dec2b3ca540b65
ashwin <ashwinbonagiri@gmail.com>
2019-03-16 18:01:33 -0400

    first commit

---
+++ src/main/java/com/ashwin/csvfileoperations/CsvfileoperationsApplication.java
@@ -0,0 +1,13 @@
+package com.ashwin.csvfileoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvfileoperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvfileoperationsApplication.class, args);
+	}
+
+}

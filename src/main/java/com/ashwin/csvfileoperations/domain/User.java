f202ab4655c37ab03432abf651a86c03c3902b7f
abonagiri <ashwinbonagiri@gmail.com>
2019-03-18 12:03:54 -0400

    Updated Validations

--- src/main/java/com/ashwin/csvfileoperations/domain/User.java
+++ src/main/java/com/ashwin/csvfileoperations/domain/User.java
@@ -1,54 +1,56 @@
-package com.ashwin.csvfileoperations.domain;
+    package com.ashwin.csvfileoperations.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+    import com.ashwin.csvfileoperations.util.CustomEmailValidator;
 
-@Entity
-public class User {
+    import javax.persistence.*;
+    import javax.validation.constraints.NotEmpty;
 
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
+    @Entity
+    public class User {
 
-    @Column
-    @NotNull(message = "{user.firstname.notempty}")
-    private String firstname;
+        @Id
+        @GeneratedValue(strategy= GenerationType.IDENTITY)
+        private Long id;
 
-    @Column
-    @NotNull(message = "{user.lastname.notempty}")
-    private String lastname;
+        @Column
+        @NotEmpty(message = "{user.firstname.notempty}")
+        private String firstname;
 
-    @Column
-    @Email(message = "{user.email.notempty}")
-    private String email;
+        @Column
+        @NotEmpty(message = "{user.lastname.notempty}")
+        private String lastname;
 
-    public String getFirstname() {
-        return firstname;
+        @Column
+        @NotEmpty
+        @CustomEmailValidator
+        //@Email(message = "{user.email.notempty}")
+        private String email;
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Long getId() {
+            return id;
+        }
     }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-}

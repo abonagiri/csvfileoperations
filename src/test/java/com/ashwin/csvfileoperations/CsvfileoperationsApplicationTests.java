b779151c2f01072dab1a91a18fd280a9a4291d66
abonagiri <ashwinbonagiri@gmail.com>
2019-03-21 21:39:08 -0400

    Junits and Mocito Updated

--- src/test/java/com/ashwin/csvfileoperations/CsvfileoperationsApplicationTests.java
+++ src/test/java/com/ashwin/csvfileoperations/CsvfileoperationsApplicationTests.java
@@ -1,16 +1,40 @@
 package com.ashwin.csvfileoperations;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.ashwin.csvfileoperations.domain.User;
+import com.ashwin.csvfileoperations.repository.UserRepository;
+import com.ashwin.csvfileoperations.service.UserServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class CsvfileoperationsApplicationTests {
 
+	@Autowired
+	UserServiceImpl userService;
+
+	@Autowired
+    UserRepository userRepository;
+
+	@DisplayName("Unit Test for Empty Users")
 	@Test
-	public void contextLoads() {
+	public void getUsersEmptyTest() {
+		assertEquals(0,userService.getUsers().size());
 	}
 
+    @DisplayName("Unit Test for Users")
+    @Test
+    public void getUsersTest() {
+
+	    User user = new User();
+        user.setEmail("x.y@z.com");
+        user.setFirstname("john");
+        user.setLastname("wick");
+	    userRepository.save(user);
+        assertEquals(1,userService.getUsers().size());
+    }
 }
+
